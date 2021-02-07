23bd0b6597cd117b26e061b65cfe6e40ef9107ef
Daniel Huber <thyvener@gmail.com>
2021-02-07 09:53:13 +0100

    Kolokwium II (testowe)

---
+++ Kolokwium II/Kolokwium II/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Kolokwium_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //przed kolokwium
+            Console.WriteLine("Testowe");
+        }
+    }
+}
